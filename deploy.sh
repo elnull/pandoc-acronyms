edc18ec1e5d28715800c13911ee07f2a4d84c95f
Mirko Boehm (Endocode) <mirko@endocode.com>
2020-02-05 10:52:24 +0100

    Simplify deployment script and remove packaging stage.
    This reduces quite some code duplication. The packaging stage ios now
    obsolete since the deploy stage always packages *and* deploys.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # See .gitlab-ci.yml for the expected variables that configure twine:
+COMMIT_DATETIME=$(git show --no-patch --no-notes --pretty='%cd' --date=format:'%Y%m%d%H%M')
+export PACKAGE_VERSION="$(git describe  --always --tags --abbrev=0 HEAD)$SUFFIX$COMMIT_DATETIME"
 echo "Packaging version $PACKAGE_VERSION for deployment..."
 python3 setup.py sdist bdist_wheel
 REPOSITORY="${TWINE_REPOSITORY_URL:-https://pypi.org}"
